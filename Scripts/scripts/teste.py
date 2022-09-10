ca1ee36acaa4bb44036dcbbc0bd6a28efc929d07
LBerto1 <LBerto1@its.jnj.com>
2022-09-10 18:11:13 -0300

    Organizando os arquivos

--- Scripts/scripts/teste.py
+++ Scripts/scripts/teste.py
@@ -6,9 +6,19 @@ from random import randrange, choice
 
 jan = Tk()
 
-
+path = os.path(__file__)
+print(path)
+imagem = PhotoImage(file=path+'\\falcao.png')
+Label(jan, image=imagem).pack()
 
 
 jan.mainloop()
 
+
+
+
+
+# lucasjonathangomes
+# Projetofatec123
+# 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
 
