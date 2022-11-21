f039982dbdd181fe6e24d9a78129adb9de3c49c7
LukasFernando <94874696+LukasFernando@users.noreply.github.com>
2022-11-20 21:48:35 -0300

    Grafico - Funcionando
    A classe GraficoInfo esta funcionando apenas para o tipo de grafico "bar"

--- scripts/main.py
+++ scripts/main.py
@@ -66,6 +66,10 @@ def Avaliacao(info:dict):
 def Historico(user):
 	return support.Historico().Retorna_historico(user) 
 
+@eel.expose 
+def Grafico_info(qual_grafico, nome_aluno=''):
+	return support.GraficoInfo().Retorna_info_pro_grafico(qual_grafico, aluno= nome_aluno)
+
 eel.start("html/login.html", port=8000)
 
 
